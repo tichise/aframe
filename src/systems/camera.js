36e13f35f851475771cd704f6eaacfc54d1a8fd8
tichise <ichise@gmail.com>
2018-06-02 22:12:48 +0900

    new

---
+++ src/systems/camera.js
@@ -0,0 +1,199 @@
+var constants = require('../constants/');
+var registerSystem = require('../core/system').registerSystem;
+
+var DEFAULT_CAMERA_ATTR = 'data-aframe-default-camera';
+
+/**
+ * Camera system. Manages which camera is active among multiple cameras in scene.
+ *
+ * @member {object} activeCameraEl - Active camera entity.
+ */
+module.exports.System = registerSystem('camera', {
+  init: function () {
+    this.activeCameraEl = null;
+    this.bindMethods();
+    // Wait for all entities to fully load before checking for existence of camera.
+    // Since entities wait for <a-assets> to load, any cameras attaching to the scene
+    // will do so asynchronously.
+    this.sceneEl.addEventListener('loaded', this.setupDefaultCamera);
+  },
+
+  bindMethods: function () {
+    this.setupDefaultCamera = this.setupDefaultCamera.bind(this);
+    this.wrapRender = this.wrapRender.bind(this);
+    this.unwrapRender = this.unwrapRender.bind(this);
+    this.render = this.render.bind(this);
+  },
+
+  /**
+   * Create a default camera if user has not added one during the initial scene traversal.
+   *
+   * Default camera offset height is at average eye level (~1.6m).
+   */
+  setupDefaultCamera: function () {
+    var sceneEl = this.sceneEl;
+    var defaultCameraEl;
+
+    // Camera already defined or the one defined it is an spectator one.
+    if (sceneEl.camera && !sceneEl.camera.el.getAttribute('camera').spectator) {
+      sceneEl.emit('camera-ready', {cameraEl: sceneEl.camera.el});
+      return;
+    }
+
+    // Set up default camera.
+    defaultCameraEl = document.createElement('a-entity');
+    defaultCameraEl.setAttribute('camera', {active: true});
+    defaultCameraEl.setAttribute('wasd-controls', '');
+    defaultCameraEl.setAttribute('look-controls', '');
+    defaultCameraEl.setAttribute(constants.AFRAME_INJECTED, '');
+    defaultCameraEl.setAttribute('position', {
+      x: 0,
+      y: constants.DEFAULT_CAMERA_HEIGHT,
+      z: 0
+    });
+    sceneEl.appendChild(defaultCameraEl);
+    sceneEl.emit('camera-ready', {cameraEl: defaultCameraEl});
+  },
+
+  /**
+   * Set a different active camera.
+   * When we choose a (sort of) random scene camera as the replacement, set its `active` to
+   * true. The camera component will call `setActiveCamera` and handle passing the torch to
+   * the new camera.
+   */
+  disableActiveCamera: function () {
+    var cameraEls = this.sceneEl.querySelectorAll('[camera]');
+    var newActiveCameraEl = cameraEls[cameraEls.length - 1];
+    newActiveCameraEl.setAttribute('camera', 'active', true);
+  },
+
+  /**
+   * Set active camera to be used by renderer.
+   * Removes the default camera (if present).
+   * Disables all other cameras in the scene.
+   *
+   * @param {Element} newCameraEl - Entity with camera component.
+   */
+  setActiveCamera: function (newCameraEl) {
+    var cameraEl;
+    var cameraEls;
+    var i;
+    var newCamera;
+    var previousCamera = this.activeCameraEl;
+    var sceneEl = this.sceneEl;
+
+    // Same camera.
+    newCamera = newCameraEl.getObject3D('camera');
+    if (!newCamera || newCameraEl === this.activeCameraEl) { return; }
+
+    // Grab the default camera.
+    var defaultCameraWrapper = sceneEl.querySelector('[' + DEFAULT_CAMERA_ATTR + ']');
+    var defaultCameraEl = defaultCameraWrapper &&
+                          defaultCameraWrapper.querySelector('[camera]');
+
+    // Remove default camera if new camera is not the default camera.
+    if (newCameraEl !== defaultCameraEl) { removeDefaultCamera(sceneEl); }
+
+    // Make new camera active.
+    this.activeCameraEl = newCameraEl;
+    this.activeCameraEl.play();
+    sceneEl.camera = newCamera;
+
+    // Disable current camera
+    if (previousCamera) {
+      previousCamera.setAttribute('camera', 'active', false);
+    }
+
+    // Disable other cameras in the scene
+    cameraEls = sceneEl.querySelectorAll('[camera]');
+    for (i = 0; i < cameraEls.length; i++) {
+      cameraEl = cameraEls[i];
+      if (!cameraEl.isEntity || newCameraEl === cameraEl) { continue; }
+      cameraEl.setAttribute('camera', 'active', false);
+      cameraEl.pause();
+    }
+    sceneEl.emit('camera-set-active', {cameraEl: newCameraEl});
+  },
+
+  /**
+   * Set spectator camera to render the scene on a 2D display.
+   *
+   * @param {Element} newCameraEl - Entity with camera component.
+   */
+  setSpectatorCamera: function (newCameraEl) {
+    var newCamera;
+    var previousCamera = this.spectatorCameraEl;
+    var sceneEl = this.sceneEl;
+    var spectatorCameraEl;
+
+    // Same camera.
+    newCamera = newCameraEl.getObject3D('camera');
+    if (!newCamera || newCameraEl === this.spectatorCameraEl) { return; }
+
+    // Disable current camera
+    if (previousCamera) {
+      previousCamera.setAttribute('camera', 'spectator', false);
+    }
+
+    spectatorCameraEl = this.spectatorCameraEl = newCameraEl;
+
+    sceneEl.addEventListener('enter-vr', this.wrapRender);
+    sceneEl.addEventListener('exit-vr', this.unwrapRender);
+
+    spectatorCameraEl.setAttribute('camera', 'active', false);
+    spectatorCameraEl.play();
+
+    sceneEl.emit('camera-set-spectator', {cameraEl: newCameraEl});
+  },
+
+  /**
+   * Disables current spectator camera.
+   */
+  disableSpectatorCamera: function () {
+    this.spectatorCameraEl = undefined;
+  },
+
+  /**
+   * Wrap the render method of the renderer to render
+   * the spectator camera after vrDisplay.submitFrame.
+   */
+  wrapRender: function () {
+    if (!this.spectatorCameraEl) { return; }
+    this.originalRender = this.sceneEl.renderer.render;
+    this.sceneEl.renderer.render = this.render;
+  },
+
+  unwrapRender: function () {
+    if (!this.originalRender) { return; }
+    this.sceneEl.renderer.render = this.originalRender;
+    this.originalRender = undefined;
+  },
+
+  render: function (scene, camera, renderTarget) {
+    var spectatorCamera;
+    var sceneEl = this.sceneEl;
+    var isVREnabled = sceneEl.renderer.vr.enabled;
+    this.originalRender.call(sceneEl.renderer, scene, camera, renderTarget);
+    if (!this.spectatorCameraEl || sceneEl.isMobile || !isVREnabled) { return; }
+    spectatorCamera = this.spectatorCameraEl.components.camera.camera;
+    sceneEl.renderer.vr.enabled = false;
+    this.originalRender.call(sceneEl.renderer, scene, spectatorCamera);
+    sceneEl.renderer.vr.enabled = isVREnabled;
+  }
+});
+
+/**
+ * Remove injected default camera from scene, if present.
+ *
+ * @param {Element} sceneEl
+ */
+function removeDefaultCamera (sceneEl) {
+  var defaultCamera;
+  var camera = sceneEl.camera;
+  if (!camera) { return; }
+
+  // Remove default camera if present.
+  defaultCamera = sceneEl.querySelector('[' + DEFAULT_CAMERA_ATTR + ']');
+  if (!defaultCamera) { return; }
+  sceneEl.removeChild(defaultCamera);
+}
---
+++ src/utils/coordinates.js
@@ -0,0 +1,96 @@
+/* global THREE */
+var debug = require('./debug');
+var extend = require('object-assign');
+
+var warn = debug('utils:coordinates:warn');
+
+// Order of coordinates parsed by coordinates.parse.
+var COORDINATE_KEYS = ['x', 'y', 'z', 'w'];
+
+// Coordinate string regex. Handles negative, positive, and decimals.
+var regex = /^\s*((-?\d*\.{0,1}\d+(e-?\d+)?)\s+){2,3}(-?\d*\.{0,1}\d+(e-?\d+)?)\s*$/;
+module.exports.regex = regex;
+
+/**
+ * Parses coordinates from an "x y z" string.
+ * Example: "3 10 -5" to {x: 3, y: 10, z: -5}.
+ *
+ * @param {string} val - An "x y z" string.
+ * @param {string} defaults - fallback value.
+ * @returns {object} An object with keys [x, y, z].
+ */
+function parse (value, defaultVec) {
+  var coordinate;
+  var vec;
+
+  if (value && value instanceof Object) {
+    var x = value.x === undefined ? defaultVec && defaultVec.x : value.x;
+    var y = value.y === undefined ? defaultVec && defaultVec.y : value.y;
+    var z = value.z === undefined ? defaultVec && defaultVec.z : value.z;
+    var w = value.w === undefined ? defaultVec && defaultVec.w : value.w;
+    if (x !== undefined) value.x = parseIfString(x);
+    if (y !== undefined) value.y = parseIfString(y);
+    if (z !== undefined) value.z = parseIfString(z);
+    if (w !== undefined) value.w = parseIfString(w);
+    return value;
+  }
+
+  if (value === null || value === undefined) {
+    return typeof defaultVec === 'object' ? extend({}, defaultVec) : defaultVec;
+  }
+
+  coordinate = value.trim().split(/\s+/g);
+
+  vec = {};
+  COORDINATE_KEYS.forEach(function (key, i) {
+    if (coordinate[i]) {
+      vec[key] = parseFloat(coordinate[i], 10);
+    } else {
+      var defaultVal = defaultVec && defaultVec[key];
+      if (defaultVal === undefined) { return; }
+      vec[key] = parseIfString(defaultVal);
+    }
+  });
+  return vec;
+}
+module.exports.parse = parse;
+
+/**
+ * Stringify coordinates from an object with keys [x y z].
+ * Example: {x: 3, y: 10, z: -5} to "3 10 -5".
+ *
+ * @param {object|string} data - An object with keys [x y z].
+ * @returns {string} An "x y z" string.
+ */
+function stringify (data) {
+  if (typeof data !== 'object') { return data; }
+  return [data.x, data.y, data.z, data.w].join(' ').trim();
+}
+module.exports.stringify = stringify;
+
+/**
+ * @returns {bool}
+ */
+function isCoordinates (value) {
+  return regex.test(value);
+}
+module.exports.isCoordinates = isCoordinates;
+
+module.exports.isCoordinate = function (value) {
+  warn('`AFRAME.utils.isCoordinate` has been renamed to `AFRAME.utils.isCoordinates`');
+  return isCoordinates(value);
+};
+
+function parseIfString (val) {
+  if (val.constructor === String) {
+    return parseFloat(val, 10);
+  }
+  return val;
+}
+
+/**
+ * Convert {x, y, z} object to three.js Vector3.
+ */
+module.exports.toVector3 = function (vec3) {
+  return new THREE.Vector3(vec3.x, vec3.y, vec3.z);
+};
